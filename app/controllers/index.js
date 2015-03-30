cc29fe5503fce7005a041697491d82ff029c69cc
Brian Runnells <brian.runnells@gmail.com>
2015-03-30 19:52:28 -0400

    Adding Applications
    Stub out css/ember logic for adding applications
    
    Need to refactor addition input into a modal

---
+++ app/controllers/index.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isHidden: true,
+  actions: {
+    addApp: function() {
+      this.set('isHidden', false);
+    },
+    saveApp: function() {
+      var name = this.get('name');
+      if (name.length > 0) {
+        var newApp = this.store.createRecord('application', {
+          name: name
+        });
+
+        newApp.save();
+        this.set('name', '');
+        this.set('isHidden', true);
+      } else {
+        // TODO - error trapping
+      }
+    }
+  }
+});
