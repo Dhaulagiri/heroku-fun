88e804bd8a4df9ab4fa21c81db61aa2f0980d1fb
Brian Runnells <brian.runnells@gmail.com>
2015-03-30 20:46:55 -0400

    Add accept header

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,7 @@ import config from '../config/environment';
 export default DS.RESTAdapter.extend({
   host: 'https://api.heroku.com',
   headers: {
+    "Accept": "application/vnd.heroku+json; version=3",
     "Authorization": "Bearer " + config.apiToken
   }
 });
